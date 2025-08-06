dedab7dc69183baa4b76ab59812c80dc8cbe7801
Dax <ukerrym@gmail.com>
2025-08-06 16:38:37 +0900

    [스택 + 큐 + 해시] 백준 - 10845 큐

--- 큐/Main.java
+++ 큐/Main.java
@@ -1,36 +1,39 @@
-//  스택
+//  큐
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(br.readLine());
-        Deque<String> stack = new ArrayDeque<>();
+        Deque<String> queue = new ArrayDeque<>();
         StringBuilder result = new StringBuilder();
 
-        while(count -- > 0) {
+        while (count-- > 0) {
             StringTokenizer input = new StringTokenizer(br.readLine(), " ");
             String cmd = input.nextToken();
 
-            switch(cmd) {
+            switch (cmd) {
                 case "push":
                     String value = input.nextToken();
-                    stack.push(value);
-                    break;
-                case "top":
-                    result.append(stack.isEmpty() ? -1 : stack.getFirst()).append("\n");
-                    break;
-                case "size":
-                    result.append(stack.size()).append("\n");
-                    break;
-                case "empty":
-                    result.append(stack.isEmpty() ? 1 : 0).append("\n");
+                    queue.addLast(value);
                     break;
                 case "pop":
-                    result.append(stack.isEmpty() ? -1 : stack.poll()).append("\n");
+                    result.append(queue.isEmpty() ? -1 : queue.poll()).append("\n");
+                    break;
+                case "size":
+                    result.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    result.append(queue.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    result.append(queue.isEmpty() ? -1 : queue.getFirst()).append("\n");
+                    break;
+                case "back":
+                    result.append(queue.isEmpty() ? -1 : queue.getLast()).append("\n");
                     break;
             }
         }
